a015d09d56cae292ac76b1969bc61dd734c3e622
a-satyateja <30695365+a-satyateja@users.noreply.github.com>
2019-04-29 11:01:28 +0530

    c

--- java/dataflow-connector-examples/src/main/java/com/google/cloud/bigtable/dataflow/example/CsvImport.java
+++ java/dataflow-connector-examples/src/main/java/com/google/cloud/bigtable/dataflow/example/CsvImport.java
@@ -113,14 +113,14 @@ public class CsvImport {
 
     @Description("The headers for the CSV file.")
     // String getHeaders();
-    @Validation.Required
+    // @Validation.Required
     ValueProvider<String> getHeaders();
 
     void setHeaders(ValueProvider<String> headers);
 
     @Description("The Cloud Storage path to the CSV file.")
     // String getInputFile();
-    @Validation.Required
+    // @Validation.Required
     ValueProvider<String> getInputFile();
 
     void setInputFile(ValueProvider<String> location);
